e618bbd91910fec55f66b77e5688f4ac66f16905
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-11-03 14:08:15 +0700

    Differentiate output dirs

--- scripts/4-run-compress-hevcb.sh
+++ scripts/4-run-compress-hevcb.sh
@@ -4,22 +4,22 @@
 mkdir -p data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/HEVC_classB/BQTerrace2_1920x1080_60Hz_8bit_P420.yuv -f image2 data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420/f%05d.png
 
-python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt \
+python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt/compress \
     --fc-hw-dim 9_16_24 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt/compress \
     --fc-hw-dim 9_16_32 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt/compress \
     --fc-hw-dim 9_16_48 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt \
+python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/BQTerrace2_1920x1080_60Hz_8bit_P420 --outf bqt/compress \
     --fc-hw-dim 9_16_64 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
@@ -28,22 +28,22 @@ python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/BQTe
 mkdir -p data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/HEVC_classB/BasketballDrive2_1920x1080_50Hz_8bit_P420.yuv -f image2 data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420/f%05d.png
 
-python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket \
+python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket/compress \
     --fc-hw-dim 9_16_24 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket/compress \
     --fc-hw-dim 9_16_32 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket/compress \
     --fc-hw-dim 9_16_48 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket \
+python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/BasketballDrive2_1920x1080_50Hz_8bit_P420 --outf basket/compress \
     --fc-hw-dim 9_16_64 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
@@ -52,22 +52,22 @@ python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Bask
 mkdir -p data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/HEVC_classB/Cactus_1920x1080_50Hz_8bit_P420.yuv -f image2 data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420/f%05d.png
 
-python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus \
+python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus/compress \
     --fc-hw-dim 9_16_24 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus/compress \
     --fc-hw-dim 9_16_32 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus/compress \
     --fc-hw-dim 9_16_48 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus \
+python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Cactus_1920x1080_50Hz_8bit_P420 --outf cactus/compress \
     --fc-hw-dim 9_16_64 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
@@ -76,22 +76,22 @@ python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Cact
 mkdir -p data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/HEVC_classB/Kimono_1920x1080_24Hz_8bit_P420.yuv -f image2 data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420/f%05d.png
 
-python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono \
+python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono/compress \
     --fc-hw-dim 9_16_24 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono/compress \
     --fc-hw-dim 9_16_32 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono/compress \
     --fc-hw-dim 9_16_48 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono \
+python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Kimono_1920x1080_24Hz_8bit_P420 --outf kimono/compress \
     --fc-hw-dim 9_16_64 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
@@ -100,22 +100,22 @@ python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/Kimo
 mkdir -p data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/HEVC_classB/ParkScene_1920x1080_24Hz_8bit_P420.yuv -f image2 data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420/f%05d.png
 
-python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park \
+python main.py -e 300 --lower-width 16 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park/compress \
     --fc-hw-dim 9_16_24 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park/compress \
     --fc-hw-dim 9_16_32 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park \
+python main.py -e 300 --lower-width 24 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park/compress \
     --fc-hw-dim 9_16_48 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
 
-python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park \
+python main.py -e 300 --lower-width 32 --num-blocks 1 --dataset data/hevc_b/ParkScene_1920x1080_24Hz_8bit_P420 --outf park/compress \
     --fc-hw-dim 9_16_64 --expansion 8 --loss Fusion6 --strides 5 3 2 2 2  --conv-type compact -b 1  --lr 0.0005 \
     --agg-ind -2 -1 1 2 --lw 0.1 --wbit 8 --t-dim 300 600 \
     --resol 1920 1080 --test-gap 150
