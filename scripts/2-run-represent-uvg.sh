06795ae8248360495c465b8fed84b972422bf9f8
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-11-03 11:28:33 +0700

    Add server path

--- scripts/2-run-represent-uvg.sh
+++ scripts/2-run-represent-uvg.sh
@@ -2,7 +2,7 @@
 
 # Beauty
 mkdir -p data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/Beauty_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Beauty_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
@@ -24,7 +24,7 @@ python main.py -e 300 --lower-width 96 --num-blocks 1 \
 
 # HoneyBee
 mkdir -p data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
@@ -46,7 +46,7 @@ python main.py -e 300 --lower-width 96 --num-blocks 1 \
 
 # Bosphorus
 mkdir -p data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
@@ -68,7 +68,7 @@ python main.py -e 300 --lower-width 96 --num-blocks 1 \
 
 # Jockey
 mkdir -p data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/Jockey_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Jockey_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
@@ -90,7 +90,7 @@ python main.py -e 300 --lower-width 96 --num-blocks 1 \
 
 # ReadySetGo
 mkdir -p data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
@@ -112,29 +112,29 @@ python main.py -e 300 --lower-width 96 --num-blocks 1 \
 
 # ShakenDry
 mkdir -p data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/ShakeNDry_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/ShakeNDry_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
-    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
+    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
-    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
+    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 6.07M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
-    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
+    --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 12.19M
 
 # YachtRide
 mkdir -p data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV
-ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/YachtRide_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV/f%05d.png
+ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/YachtRide_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
