a823b774e2c65581530efeb5a453fd8b240437a8
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-11-03 11:40:36 +0700

    Add name for output dirs

--- scripts/2-run-represent-uvg.sh
+++ scripts/2-run-represent-uvg.sh
@@ -5,19 +5,19 @@ mkdir -p data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Beauty_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
@@ -27,19 +27,19 @@ mkdir -p data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
@@ -49,19 +49,19 @@ mkdir -p data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
@@ -71,19 +71,19 @@ mkdir -p data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/Jockey_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
@@ -93,19 +93,19 @@ mkdir -p data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
@@ -115,19 +115,19 @@ mkdir -p data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/ShakeNDry_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 6.07M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
     --resol 1920 1080 # 12.19M
@@ -137,19 +137,19 @@ mkdir -p data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i /home/a100-5g/thangnv/CompressAI/examples/UVG_dataset/YachtRide_1920x1080_120fps_420_8bit_YUV.yuv -f image2 data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV/f%05d.png
 
 python main.py -e 300 --lower-width 24 --num-blocks 1 \
-    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
-    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
-    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf checkpoint \
+    --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
     --resol 1920 1080 # 12.19M
