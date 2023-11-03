0f0f52e2ee54726dbedda4b73373c347fe20b051
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-11-03 13:10:26 +0700

    Output dirs for HEVC-B

--- scripts/2-run-represent-uvg.sh
+++ scripts/2-run-represent-uvg.sh
@@ -8,19 +8,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/Beauty_1920x1080_120fps_420_8bit_YUV --outf beauty \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # HoneyBee
 mkdir -p data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV
@@ -30,19 +30,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/HoneyBee_1920x1080_120fps_420_8bit_YUV --outf honey \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # Bosphorus
 mkdir -p data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV
@@ -52,19 +52,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/Bosphorus_1920x1080_120fps_420_8bit_YUV --outf bosphorus \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # Jockey
 mkdir -p data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV
@@ -74,19 +74,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/Jockey_1920x1080_120fps_420_8bit_YUV --outf jockey \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # ReadySetGo
 mkdir -p data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV
@@ -96,19 +96,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/ReadySteadyGo_1920x1080_120fps_420_8bit_YUV --outf ready \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # ShakenDry
 mkdir -p data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV
@@ -118,19 +118,19 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
-    --resol 1920 1080 # 6.07M
+    --resol 1920 1080 --test-gap 150 # 6.07M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/ShakeNDry_1920x1080_120fps_420_8bit_YUV --outf shake \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 192 288 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
 
 # YachtRide
 mkdir -p data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV
@@ -140,16 +140,16 @@ python main.py -e 300 --lower-width 24 --num-blocks 1 \
     --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_60 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 3.09M
+    --resol 1920 1080 --test-gap 150 # 3.09M
 
 python main.py -e 300 --lower-width 32 --num-blocks 1 \
     --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_98 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 6.001M
+    --resol 1920 1080 --test-gap 150 # 6.001M
 
 python main.py -e 300 --lower-width 96 --num-blocks 1 \
     --dataset data/uvg/YachtRide_1920x1080_120fps_420_8bit_YUV --outf yatch \
     --fc-hw-dim 9_16_148 --expansion 1 --loss Fusion6 --strides 5 3 2 2 2 \
     --conv-type conv -b 1  --lr 0.0005 --agg-ind -2 -1 1 2 --lw 0.1 --t-dim 64 128 256 512 \
-    --resol 1920 1080 # 12.19M
+    --resol 1920 1080 --test-gap 150 # 12.19M
